3d69e1d5c30d0a4c6d62c3993162545b61a9030f
Дарья Фасхиева <dashaf712@mail.ru>
2022-12-08 18:05:25 +0300

    Добавьте файлы проекта.

---
+++ Faskhieva_School/Classes/DataBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faskhieva_School
+{
+    internal class DataBase
+    {
+        public static SchoolEntities Base;
+    }
+}
